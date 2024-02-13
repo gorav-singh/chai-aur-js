4d00aab08ea65851e251f2e68b46a139c49ce166
Gorav Singh <88999407+gorav-singh@users.noreply.github.com>
2024-02-13 14:19:09 +0000

    Datatypes

---
+++ 01_basics/02_datatypes.js
@@ -0,0 +1,23 @@
+"use strict";// treats all JS code as newer version
+
+//alert(3 + 3) // we are using nodejs, not browser
+
+let name = "Gorav"
+let age = 23
+let isLoggedIn = true
+
+// Data Types
+// number ==> 2 to power 53
+// bigint => 
+// string => ""
+// boolean => true/false
+// null => standalone value
+// undefined
+// symbol => unique
+
+
+// object
+
+console.log(typeof undefined); //undefined
+console.log(typeof null); //object
+
