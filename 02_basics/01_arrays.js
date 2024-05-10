3d4141479009ffb25776eb3c6c0d246ac648aecc
Gorav Singh <88999407+gorav-singh@users.noreply.github.com>
2024-05-10 07:51:54 +0000

    Arrays

---
+++ 02_basics/01_arrays.js
@@ -0,0 +1,28 @@
+//Arrays
+
+//ways to declare an array
+const myArr = [0, 1, 2, 3, 4]
+
+const Heros = ["Iron Man", "Spiderman"]
+
+const myArr2 = new Array(1, 2, 3, 4)
+
+// console.log(myArr[2])
+
+//Array Methods
+// myArr.push(6)
+// myArr.push(7)
+// myArr.pop()
+
+//myArr.unshift(8) //adds an element to start of array by shifting all the other array elements
+
+// myArr.shift()
+
+// console.log(myArr.includes(9));
+// console.log(myArr.indexOf(3));
+
+const newArr = myArr.join()
+
+console.log(myArr);
+console.log(newArr);
+ 
